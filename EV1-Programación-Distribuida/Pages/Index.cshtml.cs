215ca0de7177c56b0576bc6328652f16ecdcbf9c
FrancoLoera <theprivate_cloud@hotmail.com>
2026-08-28 08:08:11 -0600

    Inicialización del repositorio

---
+++ EV1-Programación-Distribuida/Pages/Index.cshtml.cs
@@ -0,0 +1,13 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EV1_Programación_Distribuida.Pages
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+
+        }
+    }
+}
