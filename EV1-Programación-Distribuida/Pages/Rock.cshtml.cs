02543755dd0ea80ecb4e73a8536f0fdbf3b4856d
FrancoLoera <theprivate_cloud@hotmail.com>
2026-08-30 15:00:04 -0600

    Vínculos entre páginas y artículos en proceso. Artículos de Reguetón y Jazz pendientes

---
+++ EV1-Programación-Distribuida/Pages/Rock.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace EV1_Programación_Distribuida.Pages
+{
+    public class RockModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
